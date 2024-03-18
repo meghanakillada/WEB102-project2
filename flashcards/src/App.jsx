7443857a0a38119b54f222f2b066f6c59c7605bf
maggie3000 <maggiekillada@gmail.com>
2024-03-17 20:36:43 -0700

    project part2 commit

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -6,15 +6,38 @@ const App = () => {
 
   const [currentCard, setCurrentCard] = useState(0);
   const [answer, setAnswer] = useState(false);
+  const [checkAnswer, setCheckAnswer] = useState('');
+  const [prevCard, setPrevCard] = useState(0);
 
   const changeAnswer = () => {
     (answer) ? setAnswer(false) : setAnswer(true);
+    setCheckAnswer('');
   }
 
   const showNextQuestion = () => {
     const randomIndex = Math.floor(Math.random() * infoPair.length);
+    setPrevCard(currentCard);
     setCurrentCard(randomIndex);
     setAnswer(false);
+    setCheckAnswer('');
+    guess.value = "";
+  }
+
+  const showPrevQuestion = () => {
+    setCurrentCard(prevCard);
+    setAnswer(false);
+    setCheckAnswer('');
+    guess.value = "";
+  }
+
+  const onCheckAnswer = (event) => {
+    event.preventDefault();
+    if (guess.value.toLowerCase() != infoPair[currentCard].answer.toLowerCase()){
+      setCheckAnswer('wrong');
+    }
+    else {
+      setCheckAnswer("correct");
+    }
   }
 
   return (
@@ -27,7 +50,16 @@ const App = () => {
             <h3>{(answer) ? infoPair[currentCard].answer : infoPair[currentCard].question}</h3>
         </div>
       </div>
+      <div class="container">
+        <form>
+          <label for="guess">Guess the answer here: </label>
+          <input type="text" id="guess" name="guess"></input>
+          <button type="submit" class="formbutton" onClick={onCheckAnswer}>Submit</button>
+        </form>
+      </div>
+      <h4>{checkAnswer}</h4>
       <br></br>
+      <button onClick={showPrevQuestion}>Prev</button>
       <button onClick={showNextQuestion}>Next</button>
     </div>
   )
